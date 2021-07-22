5fdf7cd6d00be7c905f52a7d39fffcca2248b8fc
Faig-Abaszada <faik.abaszada@gmail.com>
2021-07-22 23:47:19 +0400

    2.Project -  Edit Project - created and routed - id once routerde id alacagi onceden teyin olunur - ve tiklanan project-in id-si router-link icinden params olaraq routing olunur

---
+++ 2.projectplanner/src/views/EditProject.vue
@@ -0,0 +1,26 @@
+<template>
+  <form>
+    <label>Title</label>
+    <input type="text" required>
+    <input type="text"  required>
+    <label>Details</label>
+    <textarea required></textarea>
+    <textarea  required></textarea>
+    <button>Update Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: [],
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+
+  }
+}
+</script>
+
