12f9b76e119c2137769ef05455434a8bcd802a83
Faig-Abaszada <faik.abaszada@gmail.com>
2021-07-23 00:24:15 +0400

    2.Project -  Filter Navs - created

---
+++ 2.projectplanner/src/components/FilterNav.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="filter-nav">
+    <button >View all</button>
+    <button >Completed</button>
+    <button >Ongoing</button>
+  </nav>
+</template>
+
+<script>
+export default {
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+.filter-nav button {
+  background: none;
+  border: none;
+  color: #bbb;
+  outline: none;
+  font-size: 12px;
+  text-transform: uppercase;
+  margin-right: 10px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  cursor: pointer;
+}
+.filter-nav button.active {
+  color: #555;
+}
+</style>
